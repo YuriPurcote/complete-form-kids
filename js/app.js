0412750ab0ff890921030f160a2e7b46e00278b7
YuriPurcote <87339208+YuriPurcote@users.noreply.github.com>
2021-12-02 15:11:43 -0300

    Limpeza e comentarios

--- js/app.js
+++ js/app.js
@@ -1,3 +1,11 @@
+/***********************************************************
+ * AVISO :       
+ *      Todas as atividades pelo JavaScript seguem um padrão,
+ * este é formado em subsituição de classes e adição, porem a primeira atividade 
+ * é formada apartir de dois resultados onde se o resultado for o certo ele mostrará um elemento ja existente e escondido por d-none 
+ *      O desenvolvimento foi feito basicamente em DOM
+ **************************************************************/ 
+
 /* atividade 1 */
 var gol = document.getElementById('Gol');
 var gua= document.getElementById('Gua');
@@ -13,29 +21,35 @@ gol.addEventListener("click", function(e) {
 gua.addEventListener("click", function(e) {
     neutralGol.classList.add("bg-danger");
     neutralGol.classList.add("text-light");
+
     sucessGol.classList.add("d-none");
+
     neutralGol.classList.remove("bg-light");
     neutralGol.classList.remove("d-none");
 });
 gor.addEventListener("click", function(e) {
     neutralGol.classList.add("bg-danger");
     neutralGol.classList.add("text-light");
-    sucessGol.classList.add("d-none");
     neutralGol.classList.remove("bg-light");
     neutralGol.classList.remove("d-none");
+
+    sucessGol.classList.add("d-none");
+    
 });
 
 /* ATIVIDADE 2 */
 var bolinhoContainers = document.getElementById('bolinho')
 var bolinhoContainerExterno = document.getElementById('bolinhoContainer')
+
 var bolinhoSpan = document.getElementById('spanBolinho')
 var spanLibonho = document.getElementById('spanLibonho')
 var spanBonholi = document.getElementById('spanBonholi')
+
 var bo = document.getElementById('bo');
 var li= document.getElementById('li');
-var nho = document.getElementById('nho');/* certo */
+var nho = document.getElementById('nho');
+
 bo.addEventListener("click", function() {
-    // bolinho.classList.remove("d-none");
     bolinhoContainers.classList.remove("bg-success");
     bolinhoContainers.classList.add("bg-danger");
     bolinhoContainers.classList.add("text-light");
@@ -50,6 +64,7 @@ bo.addEventListener("click", function() {
 
 });
 li.addEventListener("click", function() {
+
     bolinhoContainers.classList.add("bg-danger");
     bolinhoContainers.classList.remove("bg-success");
     bolinhoContainers.classList.add("text-light");
@@ -64,6 +79,7 @@ li.addEventListener("click", function() {
     bolinhoContainerExterno.classList.add("bg-danger");
 });
 nho.addEventListener("click", function() {
+    
     bolinhoContainers.classList.remove("bg-danger");
     bolinhoContainers.classList.add("bg-success");
     bolinhoContainers.classList.add("text-light");
@@ -71,7 +87,7 @@ nho.addEventListener("click", function() {
     spanBonholi.classList.add("d-none");
     bolinhoSpan.classList.remove("d-none");
     spanLibonho.classList.add("d-none");
-    
+
     bolinhoContainerExterno.classList.remove("d-none");
     bolinhoContainerExterno.classList.add("bg-success");
     bolinhoContainerExterno.classList.remove("bg-danger");
